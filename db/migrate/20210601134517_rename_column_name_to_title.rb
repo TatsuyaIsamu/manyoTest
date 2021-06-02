0ec6ff35feeb87b36255abae59f0abf4fc8ea199
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-06-02 15:01:06 +0900

    rspec テスト記載

---
+++ db/migrate/20210601134517_rename_column_name_to_title.rb
@@ -0,0 +1,5 @@
+class RenameColumnNameToTitle < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :tasks,  :name, :title
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_01_072158) do
+ActiveRecord::Schema.define(version: 2021_06_01_134517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.text "name", null: false
+    t.text "title", null: false
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
