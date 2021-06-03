762f3d39a3757832dcf1e9765d0e09444419845a
Tatsuya Isamu <tatsuyaisamu0615@gmail.com>
2021-06-03 11:48:48 +0900

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_authenticate
-  frozen-ridge-21145
+
   def basic_authenticate
 
     authenticate_or_request_with_http_basic do |name, password|
